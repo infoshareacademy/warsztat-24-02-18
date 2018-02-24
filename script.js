578d71f6d55a4c39745e88040a86a7da94dad1c4
Mateusz Choma <sub@amazingdesign.eu>
2018-02-24 12:28:20 +0100

    Data types - primitives

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log('Hello Mateusz!')
+
+// data types in JS
+
+// primitives
+
+// string
+var textVariable = "Mateusz"
+console.log(textVariable)
+
+// number
+var numberVariable = 43
+console.log(numberVariable)
+
+// boolean
+var boolVariable = true
+console.log(boolVariable)
+
+// null
+var nullVariable = null
+console.log(nullVariable)
+
+// undefined
+var undefinedVariable = undefined
+console.log(undefinedVariable)
