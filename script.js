084a7980635315ccc41edb50ec2ace2d8dc46c80
Mateusz Choma <sub@amazingdesign.eu>
2018-02-24 12:50:22 +0100

    Data types - objects

--- script.js
+++ script.js
@@ -23,3 +23,37 @@ console.log(nullVariable)
 // undefined
 var undefinedVariable = undefined
 console.log(undefinedVariable)
+
+// objects
+
+// objects with data
+var simplestObj = {
+    name: "Mateusz",
+    lastname: "Choma"
+}
+console.log(simplestObj)
+
+// nested objects with data
+var nastedObj = {
+    name: "Mateusz",
+    car: {
+        brand: "Peugeot",
+        model: 407
+    }
+}
+console.log(nastedObj)
+console.log(nastedObj.car.model)
+console.log(nastedObj.car.year)
+
+// arrays
+var simpleArray = [
+    1,
+    {name: '"Ala"', array: [1,4,6]},
+    3,
+    4,
+    5
+]
+console.log(simpleArray)
+console.log(simpleArray[4])
+console.log(simpleArray[1].name)
+console.log(simpleArray[1]['name'])
