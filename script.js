e83c6d9e39d6f8dc9a8abe48fbec536da0d120e1
Mateusz Choma <sub@amazingdesign.eu>
2018-02-24 13:43:44 +0100

    Trying to not pollute global scope

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-var secondsCounter = 1
+function main(){
+    var secondsCounter = 1
 
-var intervalId = setInterval(
-    function(){
-        console.log(secondsCounter++)
-    },
-    1000
-)
+    var intervalId = setInterval(
+        function(){
+            console.log(secondsCounter++)
+        },
+        1000
+    )
 
-setTimeout(
-    function(){
-        clearInterval(intervalId)
-    },
-    5000
-)
+    setTimeout(
+        function(){
+            clearInterval(intervalId)
+        },
+        5000
+    )
+}
+
+main()
